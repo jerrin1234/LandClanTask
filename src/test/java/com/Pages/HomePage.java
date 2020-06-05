7daa81b0a220bcc9f4f12db7da06afdfc8b0198e
JERRIN KOTTAVILA RAJAN <jerrinkrajan@gmail.com>
2020-06-05 20:09:28 +0100

    Commiting AddUser Feature

--- src/test/java/com/Pages/HomePage.java
+++ src/test/java/com/Pages/HomePage.java
@@ -1,5 +1,21 @@
 package com.Pages;
 
-public class HomePage {
+import org.junit.Assert;
+import org.openqa.selenium.By;
 
+import com.CucumberRunner.BaseClass;
+
+public class HomePage extends BaseClass {
+	public void verifyHomePage() {
+		Assert.assertEquals("Welcome", driver.findElement(By.cssSelector(".toolbar span")).getText()); 
+	}
+public void click_on_AddUser_Button() {
+	 driver.findElement(By.cssSelector("#btnAdd")).click();
+}
+public void verify_Add_New_User_popup_window() {
+	Assert.assertEquals("Add New User", driver.findElement(By.cssSelector(".modal-title")).getText()); 
+}
+	public void verify_the_new_user_added() {
+		Assert.assertEquals("Selenium", driver.findElement(By.cssSelector("#row_3_txtSurname")).getText());  
+	}
 }
--- src/test/java/com/StepDefinitions/HomePageSD.java
+++ src/test/java/com/StepDefinitions/HomePageSD.java
@@ -1,5 +1,29 @@
 package com.StepDefinitions;
+import com.CucumberRunner.BaseClass;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
-public class HomePageSD {
+public class HomePageSD extends BaseClass{
+	@Given("^I am in homepage$")
+	public void i_am_in_homepage() throws Throwable {
+		homePage.verifyHomePage();
+	}
+
+	@When("^I click on 'AddUser' Button$")
+	public void i_click_on_AddUser_Button() throws Throwable {
+		homePage.click_on_AddUser_Button();
+	}
+	@When("^I should see the 'Add New User' popup window$")
+	public void i_should_see_the_Add_New_User_popup_window() throws Throwable {
+		homePage.verify_Add_New_User_popup_window();
+	}
+	
+	@Then("^The new user should add$")
+	public void the_new_user_should_add() throws Throwable {
+		homePage.verify_the_new_user_added();
+	}
+
+
 
 }
